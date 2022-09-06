d3277db1b66c0513b22af75000969c76d18ef724
Seth Mwebi <sethmwebi27@gmail.com>
2022-09-06 18:14:50 +0300

    fixed typescript shebang issue for stripe

--- pages/api/checkout_sessions.ts
+++ pages/api/checkout_sessions.ts
@@ -2,7 +2,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import Stripe from "stripe"
 import { urlFor } from "../../sanity"
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: "2022-08-01"
 })
 
